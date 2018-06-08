a122eb446c05509f5cbc86dc82ae0ae26ffba0ee
Romain Beauxis <toots@rastageeks.org>
2018-06-08 11:25:58 +0200

    Fix .mli

--- src/massive.mli
+++ src/massive.mli
@@ -16,8 +16,8 @@ val listFunctions : t -> string array
 val table : t -> string -> table option
 
 module Queries : sig
-  val find      : ?options:'a Js.t -> table -> 'a Js.t -> 'c Js.t array promise
-  val findOne   : ?options:'a Js.t -> table -> 'a Js.t -> 'c Js.t promise
+  val find      : ?options:'a Js.t -> table -> 'b Js.t -> 'c Js.t array promise
+  val findOne   : ?options:'a Js.t -> table -> 'b Js.t -> 'c Js.t promise
   val findOneId : ?options:'a Js.t -> table -> float -> 'b Js.t promise
   val count     : table -> 'a Js.t -> float promise
   val where     : table -> string -> int array -> 'a Js.t array promise
